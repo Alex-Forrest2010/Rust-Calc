ee642cd8a4167e1d212e56732f8c8b7b36cef1a7
Alex <blazezombie27@gmail.com>
2025-11-02 22:42:14 +0000

    idk what i did but it works now ig, i think it was disabling vsync but idek anymore

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,42 @@
 extern crate eframe;
 use eframe::egui;
 struct App {
-    output: i32,
+  output: i32,
 }
 
 impl Default for App {
-    fn default() -> Self {
-        Self{
-            output: 0,
+  fn default() -> Self {
+    Self {
+      output: 12,
+    }
+  }
+}
+
+impl eframe::App for App {
+  fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    let Self { output } = self;
+    egui::CentralPanel::default().show(ctx, |ui| {
+      ui.label(output.to_string());
+      ui.horizontal(|ui| {
+        if ui.button("1").clicked() {
+          *output += 1;
         }
-    }
+        if ui.button("2").clicked() {
+          *output += 2;
+        }
+      });
+    });
+  }
 }
 
-impl eframe::App for App{
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame){
-        let Self { output } = self;
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label(self.output.to_string());
-        });
-        
-    }
-}
-
-
-fn main() -> Result<(), eframe::Error>{
-    let options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Calculator",
-        options,
-        Box::new(|_cc| Ok(Box::new(App::default())))
-    )
+fn main() -> Result<(), eframe::Error> {
+  let options = eframe::NativeOptions {
+    vsync: false,
+    ..Default::default()
+  };
+  eframe::run_native(
+    "Calculator",
+    options,
+    Box::new(|_cc| Ok(Box::new(App::default())))
+  )
 }
